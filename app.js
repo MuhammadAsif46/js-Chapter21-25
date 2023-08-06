8324d7db0da8da66fb890f4c0a18752229f959e3
MuhammadAsif46 <asifahmed32c@gmail.com>
2023-08-07 01:18:06 +0500

    updated app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+//************ Take inputs from user and show greet fullName************/
+
+// var first = prompt("Enter a first name .");
+// var last = prompt("Enter a last name .");
+
+// var fullName = first + " " +last;
+// document.write("Welcome to ! " + fullName);
