7c1729530b8350bb65aaeed73ded8cac577af5e4
MuhammadAsif46 <asifahmed32c@gmail.com>
2023-08-07 01:18:35 +0500

    updated app.js

--- app.js
+++ app.js
@@ -5,3 +5,11 @@
 
 // var fullName = first + " " +last;
 // document.write("Welcome to ! " + fullName);
+
+
+//************ Take input from user and display length of input ************/
+
+// var mobileModel = prompt("enter your fav mobile model .");
+
+// document.write ("My Favorite Phone Is : " + mobileModel + "<br>");
+// document.write ("Length Of Strings : " + mobileModel.length);
